55e126c999b2a6b89b2b6ff788b45bdaa58ed541
plainchan <plainchan@gmail.com>
2022-04-25 15:54:01 +0800

    增加ydlidar-G4

---
+++ src/raspbot_ydlidar/bash/config_lidar_udev.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+echo  'KERNEL=="ttyUSB*", ATTRS{idVendor}=="10c4", ATTRS{idProduct}=="ea60", MODE:="0666", GROUP:="dialout",  SYMLINK+="ydlidar"' >/etc/udev/rules.d/ydlidar.rules
+
+
+service udev reload
+sleep 2
+service udev restart
+
