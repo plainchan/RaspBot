035235dbd89e05f0b002699fdebad6c6520d27f8
plainchan <plainchan@gmail.com>
2022-05-06 00:57:09 +0800

    编写bringup

--- src/raspbot_bringup/include/raspbot_bringup/raspbot_base.h
+++ src/raspbot_bringup/include/raspbot_bringup/raspbot_base.h
@@ -2,8 +2,9 @@
 #define _RASPBOT_BASE_H_
 
 #include "ros/ros.h"
+#include "sensor_msgs/Imu.h"
 #include "geometry_msgs/Twist.h"
-
+#include "serial/serial.h"
 
 namespace raspbot
 {
@@ -12,14 +13,24 @@ class BotBase
 {
 
 public:
-    BotBase();
+    BotBase(ros::NodeHandle nh,ros::NodeHandle private_nh);
     ~BotBase();
 
+    void loop();
+    int serial_init();
 
+    void raspbotTwistCallBack(const geometry_msgs::Twist::ConstPtr &msg_ptr);
+
+    ros::NodeHandle   nh_;
+    ros::NodeHandle   nhPrivate_;
     ros::Publisher    odom_pub;
     ros::Subscriber   twist_sub;
+    serial::Serial    sp;
 
-
+    std::string       base_frame;
+    std::string       odom_frame;
+    std::string       udev_port;
+    int               baud;
 
 };
 
--- src/raspbot_bringup/src/raspbot_base.cpp
+++ src/raspbot_bringup/src/raspbot_base.cpp
@@ -1,1 +1,62 @@
 #include "raspbot_bringup/raspbot_base.h"
+
+namespace raspbot
+{
+
+    BotBase::BotBase(ros::NodeHandle nh,ros::NodeHandle private_nh):
+    nh_(nh),
+    nhPrivate_(private_nh)
+    {
+        // twist_sub = nh_.subscribe<geometry_msgs::Twist>("/cmd_vel",10,raspbotTwistCallBack);
+
+        nhPrivate_.param<std::string>("base_frame",base_frame,"base_link");
+        nhPrivate_.param<std::string>("odom_frame",odom_frame,"odom");
+        nhPrivate_.param<std::string>("udev_port",udev_port,"raspbot_com_port");
+        nhPrivate_.param<int>("baud",baud,115200);
+
+        serial_init();
+    }
+    BotBase::~BotBase()
+    {
+
+    }
+
+    int BotBase::serial_init()
+    {
+        sp.setPort(udev_port);
+        sp.setBaudrate(baud);
+        try
+        {
+            sp.open();
+        }
+        catch(serial::PortNotOpenedException& e)
+        {
+            ROS_ERROR_STREAM(e.what());
+            return -1;
+        }
+
+        if(sp.isOpen())
+        {
+            ROS_INFO_STREAM("serial port opens succeed");
+            return 0;
+        }
+        else
+        {
+            ROS_INFO_STREAM("unknown error");
+            return -1;
+        }
+    }
+
+    void BotBase::loop()
+    {
+        ros::Rate r(50);
+        while(ros::ok)
+        {
+            r.sleep();
+            ros::spinOnce;
+        }
+    }
+
+
+
+}
